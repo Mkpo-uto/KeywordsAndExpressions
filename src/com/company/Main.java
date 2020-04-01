c38e437425bb2a2db5267c90573c752913ee3730
mkpou <mkpoutoe@hotmail.com>
2020-04-01 17:58:49 +1100

    scope in Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -20,5 +20,29 @@ public class Main {
                 "still more");
 
         int anotherVariable = 50; myVariable--;System.out.println("This is another one");
+
+        boolean gameOver = true;
+        int score = 5000;
+        int levelCompleted = 5;
+        int bonus = 100;
+
+        if(score <5000 && score > 1000){
+            System.out.println("Your score was less than 5000 but greater than 1000");
+        } else if (score < 1000){
+            System.out.println("Your score is less than 1000");
+        } else {
+            System.out.println("Got here");
+        }
+
+        if(gameOver == true){
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
+
+        if(gameOver == true){
+            score = 10000; levelCompleted = 8; bonus = 200;
+            int finalScore = score + (levelCompleted * bonus);
+            System.out.println("Your final score was " + finalScore);
+        }
     }
 }
