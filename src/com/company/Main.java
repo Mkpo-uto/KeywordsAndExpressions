e1bf9f53fc29d5ff0557525f4b78061cf846ed26
mkpou <mkpoutoe@hotmail.com>
2020-04-01 17:35:34 +1100

    statements, indentation and whitespace in Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,5 +10,15 @@ public class Main {
         if(highScore == 50){
             System.out.println("This is an expression");
         }
+
+        int myVariable = 50; //statement
+        myVariable++; //statement
+        myVariable--; //statement
+
+        System.out.println(" This is " +
+                "another " +
+                "still more");
+
+        int anotherVariable = 50; myVariable--;System.out.println("This is another one");
     }
 }
